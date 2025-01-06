570f7c51d42c4a53c98dc7c15cfddd25a3c3a4c6
Stefan McKinnon Edwards <sme@iysik.com>
2025-01-06 09:54:06 +0100

    feat: SqlSeverSource can point to an SQL file
    Adds `FilePath` argument to point to a text file with SQL statement,
    instead of including the entire SQL statement in the migrationsettings-file.

--- Extensions/SqlServer/Cosmos.DataTransfer.SqlServerExtension.UnitTests/SqlServerExtensionTests.cs
+++ Extensions/SqlServer/Cosmos.DataTransfer.SqlServerExtension.UnitTests/SqlServerExtensionTests.cs
@@ -1,8 +1,66 @@
+using Microsoft.Extensions.Configuration;
+using Cosmos.DataTransfer.JsonExtension.UnitTests;
+using Cosmos.DataTransfer.Interfaces;
 
 namespace Cosmos.DataTransfer.SqlServerExtension.UnitTests
 {
     [TestClass]
     public class SqlServerExtensionTests
     {
+        private Dictionary<string,string> GetBaseConfig() {
+            return new Dictionary<string, string>() {
+                { "ConnectionString", "Server" }
+            };
+        }
+
+        [TestMethod]
+        public void TestSourceSettings_ValidationOverload() 
+        {
+            var settings = TestHelpers.CreateConfig(new Dictionary<string, string>() {
+                { "", "" }
+            }).Get<SqlServerSourceSettings>();
+
+            Assert.ThrowsException<AggregateException>(() => settings.Validate());
+        }
+
+        [TestMethod]
+        public void TestSourceSettings_NeitherQueryFails() {
+            // Minimum requirement
+            var config = GetBaseConfig();
+            var settings = TestHelpers.CreateConfig(config).Get<SqlServerSourceSettings>();
+            var e = Assert.ThrowsException<AggregateException>(() => settings.Validate());
+            CollectionAssert.Contains(e.InnerExceptions.Select(x => x.Message).ToList(), 
+               "Either `QueryText` or `FilePath` are required!");
+        }
+
+        [TestMethod]
+        public void TestSourceSettings_ValidateWithQueryText() {
+            // Minimum requirement
+            var config = GetBaseConfig();
+            config["QueryText"] = "SELECT * FROM foobar;";
+            var settings = TestHelpers.CreateConfig(config).Get<SqlServerSourceSettings>();
+            settings.Validate();
+        }
+
+        [TestMethod]
+        public void TestSourceSettings_ValidateWithFilePath() {
+            // Minimum requirement
+            var config = GetBaseConfig();
+            config["FilePath"] = "file";
+            var settings = TestHelpers.CreateConfig(config).Get<SqlServerSourceSettings>();
+            settings.Validate();
+        }
+
+        [TestMethod]
+        public void TestSourceSettingsValidation_BothTextOrPathFails() 
+        {
+            var config = GetBaseConfig();
+            config["QueryText"] = "SELECT * FROM foobar;";
+            config["FilePath"] = "file";
+            var settings = TestHelpers.CreateConfig(config).Get<SqlServerSourceSettings>();
+            var e = Assert.ThrowsException<AggregateException>(() => settings.Validate());
+            CollectionAssert.Contains(e.InnerExceptions.Select(x => x.Message).ToList(), 
+                "Both `QueryText` and `FilePath` is not allowed.");
+        }
     }
 }
--- Extensions/SqlServer/Cosmos.DataTransfer.SqlServerExtension/SqlServerSourceSettings.cs
+++ Extensions/SqlServer/Cosmos.DataTransfer.SqlServerExtension/SqlServerSourceSettings.cs
@@ -10,8 +10,37 @@ namespace Cosmos.DataTransfer.SqlServerExtension
         [SensitiveValue]
         public string? ConnectionString { get; set; }
 
-        [Required]
         public string? QueryText { get; set; }
 
+        public string? FilePath { get; set; }
+
+        // Provides a class based validation on the combination of both `QueryText`
+        // and `FilePath`, as either one is required -- but both not allowed.
+        // TODO: This could benefit from a class attribute or similar, to provide
+        //   a custom validation instead of overloading the extension method's
+        //   and replicating it.
+        public void Validate() {
+            var validationErrors = this.GetValidationErrors().ToList();
+            var validation = this.validate();
+            if (validation != null) {
+                validationErrors.Add(validation);
+            }
+
+            if (validationErrors.Any())
+            {
+                throw new AggregateException($"Configuration for {this.GetType().Name} is invalid", validationErrors.Select(s => new Exception(s)));
+            }
+        }
+
+        private string? validate() {
+            if (String.IsNullOrWhiteSpace(this.QueryText) &&
+                String.IsNullOrWhiteSpace(this.FilePath)) {
+                return "Either `QueryText` or `FilePath` are required!";
+            } else if (String.IsNullOrWhiteSpace(this.QueryText) == false &&
+                String.IsNullOrWhiteSpace(this.FilePath) == false) {
+                return "Both `QueryText` and `FilePath` is not allowed.";
+            }
+            return null;
+        }
     }
 }
